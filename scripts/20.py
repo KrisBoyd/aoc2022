312c06d182226f6b58e9217248b29ab4e2f66340
bocasper2001 <boyd.caspers@nielsen.com>
2022-12-20 10:34:57 +0100

    day 20

---
+++ scripts/20.py
@@ -0,0 +1,35 @@
+from scripts.utility import read_data
+from copy import deepcopy
+
+
+def mix_file(df):
+    res = deepcopy(df)
+    for j in range(len(df)):
+        s = [x for x in res if x[0] == j][0]
+        i = res.index(s)
+        res.pop(i)
+        t = (i + s[1]) % (len(df) - 1)
+        if t == 0:
+            t = len(df)
+        res.insert(t, s)
+    return res
+
+
+def puzzles(data, decryption, times):
+    # Part 1 & 2
+    res = list(enumerate([s * decryption for s in data]))
+    for j in range(times):
+        res = mix_file(res)
+
+    res = [x[1] for x in res]
+    value = 0
+    for j in [1000, 2000, 3000]:
+        value += res[(res.index(0) + j) % len(res)]
+    print(value)
+
+
+if __name__ == '__main__':
+    day = __file__.split('/')[-1].split('.')[0]
+    data = read_data(f'data/{day}.txt', integer=True)
+    puzzles(data, decryption=1, times=1)
+    puzzles(data, decryption=811589153, times=10)
